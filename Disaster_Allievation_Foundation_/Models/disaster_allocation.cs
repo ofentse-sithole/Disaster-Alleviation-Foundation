d52397d7f55e6c6cb50b5e775ab7aa622e5968b8
ofentse-sithole-galleryX <otsithole07@gmail.com>
2023-10-18 20:38:47 +0000

    Added Model, Controller and Views of Disaster Allocation

--- Disaster_Allievation_Foundation_/Models/disaster_allocation.cs
+++ Disaster_Allievation_Foundation_/Models/disaster_allocation.cs
@@ -1,6 +1,19 @@
-﻿namespace Disaster_Allievation_Foundation_.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Disaster_Allievation_Foundation_.Models
 {
     public class disaster_allocation
     {
+        [Key]
+
+        public int Inventory_ID { get; set; }
+
+        public String Allocate_Disaster { get; set; }
+
+        public int Allocate_Money { get; set; }
+
+        public int Purchase_Amount { get; set; }
+
+        public DateTime Timestamp { get; set; }
     }
 }
