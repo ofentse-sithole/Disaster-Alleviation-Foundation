152f8224cc78282d5de26c3d2c79dc19e9106ed8
ofentse-sithole-galleryX <otsithole07@gmail.com>
2023-10-18 21:19:56 +0000

    update monetary donations authorization in controller

--- Disaster_Allievation_Foundation_/Controllers/monetary_donationsController.cs
+++ Disaster_Allievation_Foundation_/Controllers/monetary_donationsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Disaster_Allievation_Foundation_.Data;
 using Disaster_Allievation_Foundation_.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Disaster_Allievation_Foundation_.Controllers
 {
@@ -46,6 +47,7 @@ namespace Disaster_Allievation_Foundation_.Controllers
         }
 
         // GET: monetary_donations/Create
+        [Authorize]
         public IActionResult Create()
         {
             return View();
@@ -68,6 +70,7 @@ namespace Disaster_Allievation_Foundation_.Controllers
         }
 
         // GET: monetary_donations/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.monetary_donations == null)
@@ -86,6 +89,7 @@ namespace Disaster_Allievation_Foundation_.Controllers
         // POST: monetary_donations/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Monetary_ID,Donor,Date,Amount")] monetary_donations monetary_donations)
@@ -119,6 +123,7 @@ namespace Disaster_Allievation_Foundation_.Controllers
         }
 
         // GET: monetary_donations/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null || _context.monetary_donations == null)
@@ -137,6 +142,7 @@ namespace Disaster_Allievation_Foundation_.Controllers
         }
 
         // POST: monetary_donations/Delete/5
+        [Authorize]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
